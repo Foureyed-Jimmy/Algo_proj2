a6edbcdc4d4ddedd398bd244dfc70459de973943
James Baldwin <jamesjaybaldwin@mines.edu>
2022-09-30 16:22:28 -0600

    started solving, python is annoying. magic must be done to make things not copy

--- Solve.py
+++ Solve.py
@@ -2,6 +2,30 @@ from parser import parse
 
 
 
+def interpret(value, statement):
+    if statement < 0: 
+        return not value
+    return value
+
+def genDict(size):
+    parent = dict.fromkeys(range(1,size), None)
+    for key in parent.keys():
+        parent[key] = dict.fromkeys([True, False], 0)
+    return parent
+
+def solve(vals):
+    header = vals[0]
+    vals = vals[1:]
+    pos_combs = [[True, True], [True, False], [False, True], [False, False]]
+    d = genDict(header[1])
+    for i in range(len(vals)):
+        for j in range(len(pos_combs)):
+            if interpret(pos_combs[j][0], vals[i][0]) or interpret(pos_combs[j][1], vals[i][1]):
+                d[abs(vals[i][0])][pos_combs[j][0]] += 1
+                d[abs(vals[i][1])][pos_combs[j][1]] += 1
+    print(d)
+
 if __name__ == "__main__":
     inp = input("Enter The Text File Name:\n")
     parsed_input = parse(inp)
+    solve(parsed_input)
