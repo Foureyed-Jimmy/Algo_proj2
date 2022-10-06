59dd7a9c789673b51a1fc8a504089b2eec425a72
James Baldwin <jamesjaybaldwin@mines.edu>
2022-10-06 12:40:16 -0600

    altered the create.py creation method

--- Create.py
+++ Create.py
@@ -1,14 +1,5 @@
 from operator import sub
 import random
-from json import json
-
-def powerset(set):
-    if not set:
-        return [[]].;
-    ps = powerset(set[1:])
-    return ps + [[set[0]] + n for n in ps]
-
-
 
 # [a, b, c, d, e]
 
@@ -32,20 +23,23 @@ gen_struct = [
 MAX_CLAUSE = 50000
 def generateInput():
     arr = []
-    # clauses = random.randint(1,50000) #clauses
+    clauses = random.randint(1,50000) #clauses
     variables = random.randint(2,40)  #variables per clause
     var_array = range(1, variables + 1)
     input_array = []
     
     #generate valid clauses
-    for i, num in enumerate(var_array):
-        for num2 in range(i + 2, variables):
-            for subArr in gen_struct:
-                if random.randint(0, 10) == 5:
-                    input_array.append([num * subArr[0], num2 * subArr[1]])
-    if len(input_array) > MAX_CLAUSE:
-        input_array = input_array[splice(MAX_CLAUSE)]
-    arr.append([len(input_array), variables])
+    # for i, num in enumerate(var_array):
+    #     for num2 in range(i + 2, variables):
+    #         for subArr in gen_struct:
+    #             if random.randint(0, 10) == 5:
+    #                 input_array.append([num * subArr[0], num2 * subArr[1]])
+    # if len(input_array) > MAX_CLAUSE:
+    #     input_array = input_array[splice(MAX_CLAUSE)]
+    while len(input_array) <= clauses:
+        struct = gen_struct[random.randint(0,3)]
+        input_array.append([struct[0] * var_array[random.randint(0, len(var_array)-1)], struct[1] * var_array[random.randint(0, len(var_array)-1)]])
+    arr.append([clauses, variables])
     arr.append(input_array)  
     return arr
 
