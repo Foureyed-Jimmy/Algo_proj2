82631886cf04c3445912d16d8d0663d78701dd25
Kumar Chhabra <kchhabra@mines.edu>
2022-10-13 21:19:42 -0600

    Final

--- SolveBatch.py
+++ SolveBatch.py
@@ -3,7 +3,7 @@ from BatchCaller import batch
 from Validate import validate_batch
 
 def main():
-    batch("/to_solve_sets", init_solve)
+    #batch("/to_solve_sets", init_solve)
     batch("/to_validate_sets", validate_batch)
 
 if __name__ == "__main__":
